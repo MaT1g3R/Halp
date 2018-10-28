0a461876a5afc1b86b0400964951df56ac93858c
Peijun Ma <peijun.ma@protonmail.com>
2018-10-28 19:14:35 -0400

    Add activity class for request frontpage

---
+++ AndroidApp/app/src/main/java/com/csc301/team22/activity_request_frontpage.java
@@ -0,0 +1,12 @@
+package com.csc301.team22;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class activity_request_frontpage extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
