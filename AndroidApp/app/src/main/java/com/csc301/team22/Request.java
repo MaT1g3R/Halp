01b6b26c42b114471d56bcac3ea3e4695d1fa64a
Peijun Ma <peijun.ma@protonmail.com>
2018-10-28 22:58:16 -0400

    Add basic class to represent a request

---
+++ AndroidApp/app/src/main/java/com/csc301/team22/Request.java
@@ -0,0 +1,27 @@
+package com.csc301.team22;
+
+public class Request {
+    private String name;
+    private String description;
+
+    public Request(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
