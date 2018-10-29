e6358269fea692a34743bf4106b7ff5702aed264
Arjun96 <luthra.arjun@gmail.com>
2018-10-29 13:34:51 -0400

    Added the profile class as well as the corresponding activity profile

---
+++ AndroidApp/app/src/main/java/com/csc301/team22/profile.java
@@ -0,0 +1,16 @@
+package com.csc301.team22;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class profile extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+}
