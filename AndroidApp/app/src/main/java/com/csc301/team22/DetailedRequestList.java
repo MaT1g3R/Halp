8ad658203b8d01915dd892cf309ed7bdfd4d3ad2
Peijun Ma <peijun.ma@protonmail.com>
2018-10-29 21:45:49 -0400

    Finish UI for DetailedRequestList

--- AndroidApp/app/src/main/java/com/csc301/team22/DetailedRequestList.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/DetailedRequestList.java
@@ -1,13 +1,19 @@
 package com.csc301.team22;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 public class DetailedRequestList extends AppCompatActivity {
+
+    private LinearLayout linearLayoutRequestList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_request_list);
+
+        linearLayoutRequestList = findViewById(R.id.linearLayoutRequestList);
+        RequestManager.getInstance().addToLinearLayout(this, linearLayoutRequestList);
     }
 }
