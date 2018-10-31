89faa1d928dd83470d262557fe33a77dc7e57ecf
Peijun Ma <peijun.ma@protonmail.com>
2018-10-30 20:59:33 -0400

    Add blank RequestSubmissionActivity

---
+++ AndroidApp/app/src/main/java/com/csc301/team22/RequestSubmissionActivity.java
@@ -0,0 +1,13 @@
+package com.csc301.team22;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestSubmissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_submission);
+    }
+}
