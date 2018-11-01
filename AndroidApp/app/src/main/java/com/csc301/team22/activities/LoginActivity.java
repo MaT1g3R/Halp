064741c1f4dcdf032fa862c430be83571d22b1f6
lixun94 <root@DESKTOP-O03G45E.localdomain>
2018-11-01 16:59:02 -0400

    Login WIP

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.csc301.team22.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.csc301.team22.R;
+import com.csc301.team22.Util;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button buttonLogin;
+    private Button buttonCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(v -> Util.openActivity(this, PostJobFindWorkActivity.class));
+
+        Button CreateAccount = findViewById(R.id.create_account);
+        CreateAccount.setOnClickListener(v -> Util.openActivity(this, CreateAccountActivity.class));
+
+
     }
 }
