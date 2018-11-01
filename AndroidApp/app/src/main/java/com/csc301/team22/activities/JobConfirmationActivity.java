e01a706b778feb703f8686e193c14520466d96c6
Arjun96 <luthra.arjun@gmail.com>
2018-11-01 15:43:07 -0400

    Added the unified home bar with button functionality

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/JobConfirmationActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/JobConfirmationActivity.java
@@ -1,8 +1,10 @@
 package com.csc301.team22.activities;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.csc301.team22.R;
 import com.csc301.team22.Util;
@@ -10,6 +12,7 @@ import com.csc301.team22.Util;
 public class JobConfirmationActivity extends AppCompatActivity {
 
     private Button home;
+    private ImageButton profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +20,12 @@ public class JobConfirmationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_job_confirmation);
 
         home = findViewById(R.id.buttonHome);
-        home.setOnClickListener(v -> Util.openActivity(this, MainActivity.class));
+        home.setOnClickListener(v -> Util.openActivity(this, PostJobFindWorkActivity.class));
+
+        profile = findViewById(R.id.buttonProfile);
+        profile.setOnClickListener(v -> Util.openActivity(this, ProfileActivity.class));
+
+
     }
-
-
-
-
-
-
-
 
 }
