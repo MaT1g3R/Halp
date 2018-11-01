227eeec370f37bacb506b9330cbb32ce65f8e230
Peijun Ma <peijun.ma@protonmail.com>
2018-10-31 23:13:23 -0400

    Clean up some component names and colors

--- AndroidApp/app/src/main/java/com/csc301/team22/MainActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
         public void openLogin(){
-            Intent intent = new Intent(this, login.class);
+            Intent intent = new Intent(this, Login.class);
             startActivity(intent);
         }
 
