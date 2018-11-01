a63134b7c7ae86d085deda81d99fcf547dc020d7
Arjun96 <luthra.arjun@gmail.com>
2018-11-01 03:04:26 -0400

    Added button functionality

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/ProfileActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/ProfileActivity.java
@@ -2,17 +2,26 @@ package com.csc301.team22.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.csc301.team22.R;
+import com.csc301.team22.Util;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button buttonHome;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        buttonHome = findViewById(R.id.home);
+        buttonHome.setOnClickListener(v -> Util.openActivity(this, MainActivity.class));
     }
 
+
+    public void home(){Util.openActivity(this, RequestFrontpageActivity.class);}
 
 }
