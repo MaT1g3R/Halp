0fa1fbd9535f2a6c7c64e0efcbfa9a10a9f78ad5
Arjun96 <luthra.arjun@gmail.com>
2018-11-01 15:48:55 -0400

    Added profile button functionality, still needs an image

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/ProfileActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/ProfileActivity.java
@@ -1,8 +1,10 @@
 package com.csc301.team22.activities;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.csc301.team22.R;
 import com.csc301.team22.Util;
@@ -10,6 +12,7 @@ import com.csc301.team22.Util;
 public class ProfileActivity extends AppCompatActivity {
 
     private Button buttonHome;
+    private ImageButton profile;
 
 
     @Override
@@ -19,6 +22,9 @@ public class ProfileActivity extends AppCompatActivity {
 
         buttonHome = findViewById(R.id.buttonHome);
         buttonHome.setOnClickListener(v -> Util.openActivity(this, PostJobFindWorkActivity.class));
+
+        profile = findViewById(R.id.buttonProfile);
+        profile.setOnClickListener(v -> Util.openActivity(this, ProfileActivity.class));
     }
 
 
