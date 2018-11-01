2f1e3ff21bdf2366df6eca68588833ae20486d87
Arjun96 <luthra.arjun@gmail.com>
2018-11-01 12:13:33 -0400

    Added JobConfirmation page and set up the onCreate function

---
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/JobConfirmationActivity.java
@@ -0,0 +1,18 @@
+package com.csc301.team22.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.csc301.team22.R;
+
+public class JobConfirmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_confirmation);
+    }
+
+
+
+}
