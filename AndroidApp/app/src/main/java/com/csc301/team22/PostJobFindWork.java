0d63decaf3551a42fa8fadb6ab1daa4fc23a6387
Peijun Ma <peijun.ma@protonmail.com>
2018-11-01 00:09:01 -0400

    Setup links for PostJobFindWork
    Currently only RequestFrontpage is available

--- AndroidApp/app/src/main/java/com/csc301/team22/PostJobFindWork.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/PostJobFindWork.java
@@ -1,63 +1,39 @@
 package com.csc301.team22;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 public class PostJobFindWork extends AppCompatActivity {
-    private Button PostJob;
-    private Button FindWork;
-    private Button Profile;
+    private Button postJob;
+    private Button findWork;
+    private Button buttonProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_job_find_work);
 
-        PostJob = (Button) findViewById(R.id.PostJobButton);
-        PostJob.setOnClickListener(new View.OnClickListener() {
+        postJob = findViewById(R.id.PostJobButton);
+        postJob.setOnClickListener(v -> postJob());
 
-            @Override
-            public void onClick(View s) {
-                postJob();
-            }
-        });
+        findWork = findViewById(R.id.FindWorkButton);
+        findWork.setOnClickListener(v -> findWork());
 
-        FindWork = (Button) findViewById(R.id.FindWorkButton);
-        FindWork.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View s) {
-                findWork();
-            }
-        });
-
-        Profile = (Button) findViewById(R.id.FindWorkProfileButton);
-        Profile.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View s) {
-                openProfile();
-            }
-        });
+        buttonProfile = findViewById(R.id.FindWorkProfileButton);
+        buttonProfile.setOnClickListener(v -> openProfile());
     }
 
     public void postJob() {
-        Intent intent = new Intent(getApplicationContext(), RequestFrontpage.class);
-        startActivity(intent);
+        Util.openActivity(this, RequestFrontpage.class);
     }
 
     public void findWork() {
         //TODO: Jump to Work Finder page
-        Intent intent = new Intent(getApplicationContext(), RequestFrontpage.class);
-        startActivity(intent);
     }
 
     public void openProfile() {
-        //TODO: Jump to Profile page
-        Intent intent = new Intent(getApplicationContext(), RequestFrontpage.class);
-        startActivity(intent);
+        //TODO: Jump to profile page
     }
 }
