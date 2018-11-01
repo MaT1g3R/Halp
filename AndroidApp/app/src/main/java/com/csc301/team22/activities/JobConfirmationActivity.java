11964308c8575b4602db22c839f87a999b0fe8ab
Arjun96 <luthra.arjun@gmail.com>
2018-11-01 15:25:58 -0400

    Added Home button functionality

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/JobConfirmationActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/JobConfirmationActivity.java
@@ -2,16 +2,28 @@ package com.csc301.team22.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.csc301.team22.R;
+import com.csc301.team22.Util;
 
 public class JobConfirmationActivity extends AppCompatActivity {
+
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_confirmation);
+
+        home = findViewById(R.id.buttonHome);
+        home.setOnClickListener(v -> Util.openActivity(this, MainActivity.class));
     }
+
+
+
+
+
 
 
 
