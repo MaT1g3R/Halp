5b6d652669fae680c161f6190c70f92d6ca8ada0
lixun94 <root@DESKTOP-O03G45E.localdomain>
2018-11-01 17:22:23 -0400

    changed function structures in login

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/LoginActivity.java
@@ -3,7 +3,9 @@ package com.csc301.team22.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.csc301.team22.R;
 import com.csc301.team22.Util;
@@ -18,11 +20,27 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button loginButton = findViewById(R.id.login);
-        loginButton.setOnClickListener(v -> Util.openActivity(this, PostJobFindWorkActivity.class));
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
 
         Button CreateAccount = findViewById(R.id.create_account);
         CreateAccount.setOnClickListener(v -> Util.openActivity(this, CreateAccountActivity.class));
 
 
+    }
+
+    private void login(){
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+
+        // check the combination of username and password
+        // will implement in part3
+
+        Intent intent = new Intent(this, PostJobFindWorkActivity.class);
+        startActivity(intent);
     }
 }
