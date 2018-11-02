9617a27617e53c97a2a287beeb5e4d7a23614c67
Pablo Gonzalez <ph.gonzalez@mail.utoronto.ca>
2018-11-02 03:01:12 -0400

    added buttons to work accept

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/WorkAcceptActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/WorkAcceptActivity.java
@@ -2,14 +2,23 @@ package com.csc301.team22.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.csc301.team22.R;
+import com.csc301.team22.Util;
 
 public class WorkAcceptActivity extends AppCompatActivity {
+    private Button accept, decline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_accept);
+
+        accept = findViewById(R.id.acceptButton);
+        accept.setOnClickListener(v -> Util.openActivity(this, JobConfirmationActivity.class));
+
+        decline = findViewById(R.id.declineButton);
+        decline.setOnClickListener(v -> Util.openActivity(this, WorkNowWorkLaterActivity.class));
     }
 }
