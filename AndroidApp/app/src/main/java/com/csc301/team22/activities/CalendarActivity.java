27e63312e1a10dbea64a78e7254b4dbd30f22fba
Peijun Ma <peijun.ma@protonmail.com>
2018-11-02 12:46:46 -0400

    Link help later button in request submission
    Also passes request title and description to the calandar page using a
    bundle

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/CalendarActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/CalendarActivity.java
@@ -1,9 +1,8 @@
 package com.csc301.team22.activities;
 
 import android.app.TimePickerDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
@@ -14,68 +13,69 @@ import com.csc301.team22.Util;
 import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity {
-    private Button submit;
     EditText chooseTime, pickTime;
     TimePickerDialog timePickerDialog;
     Calendar calendar;
     int currentHour;
     int currentMinute;
     String amPm;
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+        Bundle extras = getIntent().getExtras();
+        assert extras != null;
+        String title = extras.getString("title");
+        String description = extras.getString("description");
+        assert title != null;
+        assert description != null;
+
         submit = findViewById(R.id.submitButton);
         submit.setOnClickListener(v -> Util.openActivity(this, JobDescriptionActivity.class));
 
         chooseTime = findViewById(R.id.editText10);
-        chooseTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calendar = Calendar.getInstance();
-                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-                currentMinute = calendar.get(Calendar.MINUTE);
+        chooseTime.setOnClickListener(view -> {
+            calendar = Calendar.getInstance();
+            currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+            currentMinute = calendar.get(Calendar.MINUTE);
 
-                timePickerDialog = new TimePickerDialog(CalendarActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
-                        if (hourOfDay >= 12) {
-                            amPm = "PM";
-                        } else {
-                            amPm = "AM";
-                        }
-                        chooseTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+            timePickerDialog = new TimePickerDialog(CalendarActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                    if (hourOfDay >= 12) {
+                        amPm = "PM";
+                    } else {
+                        amPm = "AM";
                     }
-                }, currentHour, currentMinute, false);
+                    chooseTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                }
+            }, currentHour, currentMinute, false);
 
-                timePickerDialog.show();
-            }
+            timePickerDialog.show();
         });
 
         pickTime = findViewById(R.id.editText11);
-        pickTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calendar = Calendar.getInstance();
-                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-                currentMinute = calendar.get(Calendar.MINUTE);
+        pickTime.setOnClickListener(view -> {
+            calendar = Calendar.getInstance();
+            currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+            currentMinute = calendar.get(Calendar.MINUTE);
 
-                timePickerDialog = new TimePickerDialog(CalendarActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
-                        if (hourOfDay >= 12) {
-                            amPm = "PM";
-                        } else {
-                            amPm = "AM";
-                        }
-                        pickTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+            timePickerDialog = new TimePickerDialog(CalendarActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                    if (hourOfDay >= 12) {
+                        amPm = "PM";
+                    } else {
+                        amPm = "AM";
                     }
-                }, currentHour, currentMinute, false);
+                    pickTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                }
+            }, currentHour, currentMinute, false);
 
-                timePickerDialog.show();
-            }
+            timePickerDialog.show();
         });
     }
 }
