0ed50bf0c191737ecf23d191fc6bfa9da9546bda
Brandon Cormier <Brandon.cormier@mail.utoronto.ca>
2018-11-26 13:30:31 -0500

    added Unirest to build.gradle

--- AndroidApp/app/src/main/java/com/csc301/team22/Util.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/Util.java
@@ -7,9 +7,6 @@ import android.widget.EditText;
 
 public final class Util {
 
-    private Util() {
-    }
-
     public static String getEditTextString(EditText editText, boolean reset) {
         String r = editText.getText().toString();
         if (reset) {
