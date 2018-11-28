7989fa82b9c254bc8611d8a80218696459b06a1b
Peijun Ma <peijun.ma@protonmail.com>
2018-11-28 01:38:33 -0500

    Refactor view to use controller

--- Backend/halp_backend/views.py
+++ Backend/halp_backend/views.py
@@ -1,26 +1,15 @@
 from django.http import HttpRequest, JsonResponse
 
-from halp_backend import user_converter
+from halp_backend import controller, user_converter
 from halp_backend.auth import require_auth
 from halp_backend.models import User
 from halp_backend.util import allow_methods
 
 
-@allow_methods(methods=['GET'])
+@allow_methods(methods={'GET'})
 @require_auth
 def profile(user: User, request: HttpRequest):
     user_id = request.GET.get('user_id')
     if user_id is None:
         return JsonResponse(user_converter.to_dict(user))
-
-    try:
-        user_id = int(user_id)
-    except (TypeError, ValueError):
-        return JsonResponse({'error': f'{user_id} is not a valid user ID'}, status=400)
-
-    try:
-        found_user = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return JsonResponse({'error': f'User with ID {user_id} does not exist'}, status=400)
-
-    return JsonResponse(user_converter.to_dict(found_user))
+    return controller.get_profile(user_id)
