aecbd5b28fff60b288570769f3f04ad1c88766ec
Peijun Ma <peijun.ma@protonmail.com>
2018-11-28 00:26:27 -0500

    Add helper function to encode auth

--- Backend/halp_backend/tests/conftest.py
+++ Backend/halp_backend/tests/conftest.py
@@ -1,3 +1,5 @@
+from base64 import b64encode
+
 import pytest
 from django.test.client import RequestFactory
 
@@ -5,3 +7,7 @@ from django.test.client import RequestFactory
 @pytest.fixture()
 def request_factory():
     return RequestFactory()
+
+
+def encode_auth(email, password):
+    return b64encode(f'Basic {email}:{password}'.encode()).decode()
--- Backend/halp_backend/tests/test_auth.py
+++ Backend/halp_backend/tests/test_auth.py
@@ -4,6 +4,7 @@ import pytest
 
 from halp_backend.auth import authenticate_user
 from halp_backend.models import User
+from halp_backend.tests.conftest import encode_auth
 
 pytestmark = pytest.mark.django_db
 
@@ -15,7 +16,7 @@ def test_success(email, password, request_factory):
     user = User.objects.create_user(email, password)
     user.save()
     request = request_factory.get('/')
-    request.META['HTTP_AUTHORIZATION'] = b64encode(f'Basic {email}:{password}'.encode()).decode()
+    request.META['HTTP_AUTHORIZATION'] = encode_auth(email, password)
     assert user == authenticate_user(request).unwrap()
 
 
@@ -26,7 +27,7 @@ def test_bad_header(email, password, request_factory):
     user = User.objects.create_user(email, password)
     user.save()
     request = request_factory.get('/')
-    request.META['AUTHORIZATION'] = b64encode(f'Basic {email}:{password}'.encode()).decode()
+    request.META['AUTHORIZATION'] = encode_auth(email, password)
     assert authenticate_user(request).is_none
 
 
@@ -37,7 +38,7 @@ def test_bad_password(email, password, request_factory):
     user = User.objects.create_user(email, password)
     user.save()
     request = request_factory.get('/')
-    request.META['HTTP_AUTHORIZATION'] = b64encode(f'Basic {email}:{password}a'.encode()).decode()
+    request.META['HTTP_AUTHORIZATION'] = encode_auth(email, password + 'a')
     assert authenticate_user(request).is_none
 
 
@@ -48,7 +49,7 @@ def test_bad_email(email, password, request_factory):
     user = User.objects.create_user(email, password)
     user.save()
     request = request_factory.get('/')
-    request.META['HTTP_AUTHORIZATION'] = b64encode(f'Basic c{email}:{password}'.encode()).decode()
+    request.META['HTTP_AUTHORIZATION'] = encode_auth(email + 'c', password)
     assert authenticate_user(request).is_none
 
 
@@ -74,6 +75,5 @@ def test_bad_encode(email, password, request_factory):
     user = User.objects.create_user(email, password)
     user.save()
     request = request_factory.get('/')
-    request.META['HTTP_AUTHORIZATION'] = b64encode(
-        f'Basic {email}:{password}'.encode()).decode()[1:]
+    request.META['HTTP_AUTHORIZATION'] = encode_auth(email, password)[1:]
     assert authenticate_user(request).is_none
