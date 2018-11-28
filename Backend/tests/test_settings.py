fae2bc544e0f82dbb3a8219059cc2f3a87436d1a
Peijun Ma <peijun.ma@protonmail.com>
2018-11-27 22:49:57 -0500

    Make the test db in memory

--- Backend/tests/test_settings.py
+++ Backend/tests/test_settings.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'TEST': {
-            'NAME': 'memory'
+            'NAME': ':memory:'
         }
     }
 }
