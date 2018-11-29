443da62fae2a43d42657c9d13474eacc24d32036
Limprofit <phg97@hotmail.com>
2018-11-29 14:13:02 -0500

    finished create account activity

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/CreateAccountActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/CreateAccountActivity.java
@@ -2,44 +2,30 @@ package com.csc301.team22.activities;
 
 import com.csc301.team22.api.*;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.csc301.team22.R;
 
-import java.util.Iterator;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
     String first_name, last_name, email, pass, re_pass;
     String error = "Passwords must match";
+    String emptyError = "All fields must be filled";
     MockHTTPAdapter mock = MockHTTPAdapter.getInstance();
-
-    public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String TEXT = "text";
-    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         
-        Button create = (Button) findViewById(R.id.button3);
+        Button create = findViewById(R.id.button3);
 
-        create.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                create_account();
-            }
-        });
-
-//        loadData();
-//        updateViews();
+        create.setOnClickListener(v -> create_account());
     }
 
     public void create_account(){
@@ -58,8 +44,12 @@ public class CreateAccountActivity extends AppCompatActivity {
         pass = password.getText().toString();
         re_pass = re_password.getText().toString();
 
-        if (!pass.equals(re_pass)) {
+        // Checking if entries are valid
+        if (!re_pass.equals(pass)) {
             wrong.setText(error);
+        } else if (pass.equals("") || first_name.equals("")
+                || last_name.equals("") || email.equals("")) {
+            wrong.setText(emptyError);
         } else {
 
             CreateUser newUser = new CreateUser.Builder().first_name(first_name)
@@ -69,7 +59,7 @@ public class CreateAccountActivity extends AppCompatActivity {
 
             Intent intent = new Intent(this, ProfileActivity.class);
 
-            // Test
+            // Passing user_id to next activity
             intent.putExtra("CREATEID", nUser.getUser_id());
 
             startActivity(intent);
