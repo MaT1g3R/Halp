8e485d0b59cdc105e4c2ccdaf4d3141afde80fc8
Peijun Ma <peijun.ma@protonmail.com>
2018-11-28 23:28:51 -0500

    Add json validation

--- Backend/halp_backend/util.py
+++ Backend/halp_backend/util.py
@@ -1,7 +1,10 @@
+import json
 from functools import partial, wraps
+from typing import AnyStr, Dict, List, Union
 
 from django.http import HttpRequest, JsonResponse
-from option import NONE, Option, Some
+from jsonschema import ValidationError, validate
+from option import Err, NONE, Ok, Option, Result, Some
 
 
 def get_http_header(reqeust: HttpRequest, header: str) -> Option[str]:
@@ -41,6 +44,16 @@ def allow_methods(methods, view=None):
 
 
 def validate_int(x) -> Option[int]:
+    """
+    Try to convert some value to a int
+
+    Args:
+        x: The value
+
+    Returns:
+        Some(int) if the conversion is successful
+        NONE      otherwise
+    """
     if isinstance(x, (str, int)):
         try:
             parsed_int = int(x)
@@ -49,3 +62,38 @@ def validate_int(x) -> Option[int]:
         else:
             return Some(parsed_int)
     return NONE
+
+
+def load_json(string: AnyStr) -> Union[Option[Dict], Option[List]]:
+    """
+    Try to load a JSON string
+
+    Args:
+        string: The string to be loaded
+
+    Returns:
+        Some(json) if the load is successful
+        NONE       otherwise
+    """
+    try:
+        js = json.loads(string)
+    except (TypeError, json.JSONDecodeError):
+        return NONE
+    return Some(js)
+
+
+def validate_json(json_data, schema: Dict, *args, **kwargs) -> Result[None, str]:
+    """Validate a JSON value"""
+    try:
+        validate(json_data, schema, *args, **kwargs)
+    except ValidationError as e:
+        return Err(e.message)
+    return Ok(None)
+
+
+def validate_json_string(string: AnyStr, schema: Dict, *args, **kwargs) -> Result[None, str]:
+    """Parse a JSON string and validate it"""
+    parsed_json = load_json(string)
+    if not parsed_json:
+        return Err(f'{string} is not a valid json string')
+    return validate_json(parsed_json.value, schema, *args, **kwargs)
