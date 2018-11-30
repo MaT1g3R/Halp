1105669d49beb94a20ae88d309132b8d9d5d3ad7
Arjun96 <luthra.arjun@gmail.com>
2018-11-30 16:14:02 -0500

    Removing commented out code

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/LoginActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/LoginActivity.java
@@ -38,16 +38,6 @@ public class LoginActivity extends AppCompatActivity {
     private String email = "", password = "", emptyError = "All fields must be filled";
     private String error = "Wrong email or password";
 
-//    public class BagOfPrimitives {
-//        public int value1 = 1;
-//        public String value2 = "hi";
-//        private transient int value3 = 3;
-//        BagOfPrimitives() {
-//
-//            // no-args constructor
-//        }
-//    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,56 +83,4 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-
-
-//    private void login() {
-//        EditText username = findViewById(R.id.username);
-//        EditText password = findViewById(R.id.password);
-//
-//        // check the combination of username and password
-//        // will implement in part3
-//
-//        Thread thread = new Thread(new Runnable() {
-//
-//            @Override
-//            public void run() {
-//                try {
-//                    MediaType JSON
-//                            = MediaType.parse("application/json; charset=utf-8");
-//
-//
-//                    BagOfPrimitives obj = new BagOfPrimitives();
-//
-//                    String json = new Gson().toJson(obj);
-//                    assert json != null;
-//
-//
-//                    OkHttpClient client = new OkHttpClient();
-//
-//                    RequestBody body = RequestBody.create(JSON, json);
-//                    Request request = new Request.Builder()
-//                            .url("https://httpbin.org/post")
-//                            .post(body)
-//                            .build();
-//                    Response response = client.newCall(request).execute();
-//                    String resp_json = response.body().string();
-//                    JSONObject Jobject = new JSONObject(resp_json);
-//                    JSONObject hi = Jobject.getJSONObject("json");
-//                    BagOfPrimitives respObj = new Gson().fromJson(hi.toString(), BagOfPrimitives.class);
-//                    System.out.println(respObj.value1);
-//                    System.out.println(respObj.value2);
-//
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//
-//        thread.start();
-//
-//
-//        Intent intent = new Intent(this, PostJobFindWorkActivity.class);
-//        startActivity(intent);
-//    }
 }
