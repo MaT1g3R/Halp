7864a48f454fa1d13c81d968302b6357eb2e2930
Brandon Cormier <Brandon.cormier@mail.utoronto.ca>
2018-11-29 20:15:58 -0500

    added location permissions and a location permission request on the login screen. Set up the emulator gps and adjusted the zoom and tracking of the map.

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/WorkerFoundConfirmationActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/WorkerFoundConfirmationActivity.java
@@ -1,12 +1,17 @@
 package com.csc301.team22.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import com.csc301.team22.R;
 import com.csc301.team22.Util;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -30,10 +35,18 @@ public class WorkerFoundConfirmationActivity extends AppCompatActivity implement
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in worker, and move the camera.
+        LatLng worker = new LatLng(43.66, -79.4);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        mMap.addMarker(new MarkerOptions().position(worker).title("Marker for Worker"));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(worker));
     }
 }
 
