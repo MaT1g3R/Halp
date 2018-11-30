6df619213345188aae9d78eaa5e7c254400258a6
Brandon Cormier <Brandon.cormier@mail.utoronto.ca>
2018-11-29 19:07:58 -0500

    worked out some bugs and got the map working in found WorkerFoundConfirmationActivity.java. Now I just need to read over the documentation so I can get the locations working correctly

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/WorkerFoundConfirmationActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/WorkerFoundConfirmationActivity.java
@@ -1,16 +1,41 @@
 package com.csc301.team22.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import com.csc301.team22.R;
 import com.csc301.team22.Util;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class WorkerFoundConfirmationActivity extends AppCompatActivity {
+public class WorkerFoundConfirmationActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker_found_confirmation);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
+
+
+
