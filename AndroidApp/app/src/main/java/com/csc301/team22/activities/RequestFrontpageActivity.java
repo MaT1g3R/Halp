c7d72e83401443ca775c89c46fc129a992d7695f
Arjun96 <luthra.arjun@gmail.com>
2018-11-30 16:14:48 -0500

    Removing commented out code

--- AndroidApp/app/src/main/java/com/csc301/team22/activities/RequestFrontpageActivity.java
+++ AndroidApp/app/src/main/java/com/csc301/team22/activities/RequestFrontpageActivity.java
@@ -3,14 +3,9 @@ package com.csc301.team22.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 import com.csc301.team22.R;
 import com.csc301.team22.Util;
-import com.csc301.team22.api.*;
 
 public class RequestFrontpageActivity extends AppCompatActivity {
     private Button buttonPostRequest;
@@ -27,22 +22,5 @@ public class RequestFrontpageActivity extends AppCompatActivity {
         buttonCurrentRequests = findViewById(R.id.buttonCurrentRequests);
         buttonCurrentRequests.setOnClickListener(v -> Util.openActivity(this, DetailedRequestListActivity.class));
 
-//        Request request = new Request.Builder().request_id(2).build();
-//
-//        ScrollView scroll = findViewById(R.id.frontScroll);
-//        LinearLayout ll = new LinearLayout(this);
-//
-//        ll.setOrientation(LinearLayout.VERTICAL);
-//        scroll.addView(ll);
-//
-//        TextView tv = new TextView(this);
-//        tv.setText("Test");
-//        ll.addView(tv);
-//
-////        EditText et = new EditText(this);
-////        et.setText("ET test");
-////        ll.addView(et);
-//
-////        this.setContentView(scroll);
     }
 }
