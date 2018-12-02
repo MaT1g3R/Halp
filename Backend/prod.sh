6ed51668506eef33d488947baed3e5c85a5a77b1
MaT1g3R <mat1g3r@gmail.com>
2018-12-01 19:40:03 -0500

    Update deployment

--- Backend/prod.sh
+++ Backend/prod.sh
@@ -3,4 +3,4 @@ set -e
 poetry run python manage.py check --fail-level WARNING --deploy
 poetry run python manage.py makemigrations
 poetry run python manage.py migrate
-poetry run gunicorn halp.wsgi
+poetry run gunicorn halp.wsgi -b 0.0.0.0:8000
